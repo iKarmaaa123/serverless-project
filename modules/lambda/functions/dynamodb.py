6bb551b694383ab998bdf43e9f20dde0b6f52070
iKarmaa123 <waael@hotmail.co.uk>
2026-07-21 22:29:18 +0100

    fix: added extra condition for a statusCode 200 for when the other conditions are false

--- modules/lambda/functions/dynamodb.py
+++ modules/lambda/functions/dynamodb.py
@@ -18,6 +18,10 @@ def _validate_request(path: str, query_string: Dict[str, str], user: str):
 
     elif not user:
         return {"statusCode": 400, "body": json.dumps("Invalid path parameter was passed in")}
+    
+    else:
+        return {"statusCode": 200}
+        
 
 def _sending_to_dynamodb(user, table_name):
     try:
