1d8416941e8b8863481b1b99d1ed1bf1094855f0
iKarmaa123 <waael@hotmail.co.uk>
2026-07-25 00:03:28 +0100

    fix: fixing formatting due to failed terraform -recursive check step

--- modules/sqs/sqs.tf
+++ modules/sqs/sqs.tf
@@ -1,8 +1,8 @@
 resource "aws_sqs_queue" "s3_lambda_deadletter_queue" {
-  name                      = var.s3_lambda_deadletter_queue_name
-  delay_seconds             = var.delay_seconds
-  max_message_size          = var.max_message_size
-  message_retention_seconds = var.message_retention_seconds
-  receive_wait_time_seconds = var.receive_wait_time_seconds
+  name                       = var.s3_lambda_deadletter_queue_name
+  delay_seconds              = var.delay_seconds
+  max_message_size           = var.max_message_size
+  message_retention_seconds  = var.message_retention_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
   visibility_timeout_seconds = 30
 }
